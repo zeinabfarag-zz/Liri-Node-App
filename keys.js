aaa5b58f67c3d61fb6694c296bb9ad432082a347
Zee <zzeinabfaragg@gmail.com>
2019-01-27 21:26:24 -0500

    touchup

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
+//console.log("this is loaded");
+
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -8,8 +10,4 @@ exports.twitter = {
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
-};
-
-exports.omdb = {
-  id: process.env.OMDB_KEY
 };
--- liri.js
+++ liri.js
@@ -1,145 +1,207 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-var Twitter = require("twitter");
-var Spotify = require("node-spotify-api");
 var fs = require("fs");
-var request = require("request");
 
-var spotify = new Spotify(keys.spotify);
+/* 8. At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package: */
+require("dotenv").config();
 
-var client = new Twitter(keys.twitter);
+/* 9. Add the code required to import the `keys.js` file and store it in a variable. */
+const keys = require("./keys");
 
-var API = process.argv[2];
-var input = process.argv[3];
+/* 10. Make it so liri.js can take in one of the following commands:
+    * `my-tweets`
+    * `spotify-this-song`
+    * `movie-this`
+    * `do-what-it-says` */
+run(process.argv[2], process.argv[3]);
 
-function cases() {
-  switch (API) {
+function run(command, argument) {
+  switch (command) {
     case "my-tweets":
-      twitter();
+      /*  1. `node liri.js my-tweets` */
+      myTweets();
       break;
-
     case "spotify-this-song":
-      if (!input) {
-        input = "The Sign";
-      }
-      spotifySong(input);
+      /*  2. `node liri.js spotify-this-song '<song name here>'` */
+      spotifyThisSong(argument);
       break;
-
     case "movie-this":
-      if (!input) {
-        input = "Mr.Nobody";
-      }
-      movie(input);
+      /*  3. `node liri.js movie-this '<movie name here>'` */
+      movieThis(argument);
       break;
-
     case "do-what-it-says":
-      readFile();
+      /*   4. `node liri.js do-what-it-says` */
+      doWhatItSays();
       break;
   }
 }
 
-cases();
+/*  This will show your last 20 tweets and when they were created at in your terminal/bash window. */
+function myTweets() {
+  var Twitter = require("twitter");
+  var client = new Twitter(keys.twitter);
+  var log = [];
 
-function twitter() {
-  var params = { screen_name: "nodejs" };
+  var params = {
+    screen_name: "@davidpham",
+    count: 20
+  };
 
-  client.get("statuses/user_timeline", params, function(error, tweets) {
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
     if (!error) {
-      for (var i = 0; i < 20; i++) {
-        var texts = JSON.stringify(tweets[i].text);
-        var date = JSON.stringify(tweets[i].created_at);
-
-        console.log(texts);
-        console.log(date);
-
-        appendFile(API);
-        appendFile(texts);
-        appendFile(date);
+      for (element in tweets) {
+        var tweetText = tweets[element].text;
+        log.push(tweetText + "\n");
+        console.log(tweetText);
       }
     }
+    writeLog(log);
   });
 }
 
-function spotifySong(song) {
-  spotify.search({ type: "track", query: song }, function(err, data) {
+/*  This will show the following information about the song in your terminal/bash window
+        * Artist(s)
+        * The song's name
+        * A preview link of the song from Spotify
+        * The album that the song is from */
+
+/*  If no song is provided then your program will default to "The Sign" by Ace of Base. */
+function spotifyThisSong(songName = "The Sign") {
+  var Spotify = require("node-spotify-api");
+  var spotify = new Spotify(keys.spotify);
+  var log = [];
+
+  spotify.search({ type: "track", query: songName, limit: 1 }, function(
+    err,
+    data
+  ) {
     if (err) {
       return console.log("Error occurred: " + err);
     }
-    var artist = JSON.stringify(data.tracks.items[0].album.artists[0].name);
 
-    var album = JSON.stringify(data.tracks.items[0].album.name);
+    console.log("Artist[s]:");
 
-    var name = JSON.stringify(data.tracks.items[0].name);
+    for (element in data.tracks.items[0].artists) {
+      var artistName = data.tracks.items[0].artists[element].name;
+      console.log(artistName);
+      log.push("Artist: " + artistName + "\n");
+    }
 
-    var link = JSON.stringify(
-      data.tracks.items[0].artists[0].external_urls.spotify
-    );
+    var sName = data.tracks.items[0].name;
+    log.push("Song Name: " + sName + "\n");
+    console.log("Song Name: " + sName);
 
-    console.log("name: " + name);
-    console.log("artist: " + artist);
-    console.log("album: " + album);
-    console.log("preview link: " + link);
+    var previewURL = data.tracks.items[0].preview_url;
+    log.push("Preview Link: " + previewURL + "\n");
+    console.log("Preview Link: " + previewURL);
 
-    appendFile(API);
-    appendFile(artist);
-    appendFile(album);
-    appendFile(link);
+    var album = data.tracks.items[0].album.name;
+    log.push("Album: " + album + "\n");
+    console.log("Album: " + album);
+
+    writeLog(log);
   });
 }
 
-function movie(title) {
-  var key = keys.omdb.id;
-  request(
-    "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=" + key,
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var movietitle = JSON.parse(body).Title;
-        var imdb = JSON.parse(body).imdbRating;
-        var rtomatoes = JSON.parse(body).Ratings[1].Value;
-        var country = JSON.parse(body).Country;
-        var language = JSON.parse(body).Language;
-        var plot = JSON.parse(body).Plot;
-        var actors = JSON.parse(body).Actors;
+/*  This will output the following information to your terminal/bash window:
+        * Title of the movie.
+        * Year the movie came out.
+        * IMDB Rating of the movie.
+        * Rotten Tomatoes Rating of the movie.
+        * Country where the movie was produced.
+        * Language of the movie.
+        * Plot of the movie.
+        * Actors in the movie. */
 
-        console.log("Title: " + movietitle);
-        console.log("IMDB rating: " + imdb);
-        console.log("Rotten Tomatoes rating: " + rtomatoes);
-        console.log("Country Produced: " + country);
-        console.log("Language: " + language);
-        console.log("Plot: " + plot);
-        console.log("Actors: " + actors);
+/*  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.' */
 
-        appendFile(API);
-        appendFile(movietitle);
-        appendFile(imdb);
-        appendFile(rtomatoes);
-        appendFile(country);
-        appendFile(language);
-        appendFile(plot);
-        appendFile(actors);
+/*  * You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`. */
+function movieThis(movieName = "Mr. Nobody.") {
+  var request = require("request");
+
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  request(queryUrl, function(error, response, body) {
+    var log = [];
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      var title = JSON.parse(body).Title;
+      console.log("Title: " + title);
+      log.push("Title: " + title + "\n");
+
+      var releaseYear = JSON.parse(body).Year;
+      console.log("Release Year: " + releaseYear);
+      log.push("Year: " + releaseYear + "\n");
+
+      var imdbRating = JSON.parse(body).imdbRating;
+      console.log("IMDB Rating: " + imdbRating);
+      log.push("IMDB Rating: " + imdbRating + "\n");
+
+      for (element in JSON.parse(body).Ratings) {
+        if (JSON.parse(body).Ratings[element].Source === "Rotten Tomatoes") {
+          var rottenTomatoesRating = JSON.parse(body).Ratings[element].Value;
+          console.log("Rotten Tomatoes Rating: " + rottenTomatoesRating);
+          log.push("Rotten Tomatoes Rating: " + rottenTomatoesRating + "\n");
+        }
       }
-    }
-  );
-}
 
-function readFile() {
-  fs.readFile("./random.txt", "utf8", (err, data) => {
-    appendFile(API);
+      var country = JSON.parse(body).Country;
+      console.log("Country[ies]: " + country);
+      log.push("Country[ies]: " + country + "\n");
 
-    if (err) throw err;
-    array = data.split(",");
-    if (array[0]) {
-      API = array[0];
+      var language = JSON.parse(body).Language;
+      console.log("Language[s]:" + language);
+      log.push("Language[s]: " + language + "\n");
+
+      var plot = JSON.parse(body).Plot;
+      console.log("Plot: " + plot);
+      log.push("Plot: " + plot + "\n");
+
+      var actors = JSON.parse(body).Actors;
+      console.log("Actor[s]: " + actors);
+      log.push("Actors: " + actors + "\n");
+
+      writeLog(log);
     }
-    if (array[1]) {
-      input = array[1];
-    }
-    cases();
   });
 }
 
-function appendFile(append_data) {
-  fs.appendFile("log.txt", append_data, err => {
-    if (err) throw err;
+/*   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    
+    * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+    
+    * Feel free to change the text in that document to test out the feature for other commands. */
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+
+    var dataArr = data.split(",");
+    var command = dataArr[0];
+    var argument = dataArr[1];
+    run(command, argument);
   });
+}
+
+/* ### BONUS
+
+* In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
+
+* Make sure you append each command you run to the `log.txt` file. 
+
+* Do not overwrite your file each time you run a command. */
+function writeLog(log) {
+  for (element in log) {
+    fs.appendFile("log.txt", log[element], function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
 }
